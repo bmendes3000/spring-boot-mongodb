d3a20e5cdaabbeaa5b2d986e7a6dd624ddb42e19
Bruno Mendes <bmendes3000@gmail.com>
2017-02-14 16:35:35 -0200

    Project create

---
+++ src/main/java/org/br/spring/mongo/Application.java
@@ -0,0 +1,17 @@
+package org.br.spring.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Class Main to start project.
+ * @author Bruno Mendes
+ * @since 1.0
+ * @version 1.0
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
